25bc64cabde376461c04376a421df49907ef9b57
KwabSA <kwabsa.dev@gmail.com>
2024-07-03 11:06:18 -0400

    Added Practice Task: Reset functionality

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,12 +3,19 @@ let count = 0;
 const increaseCount = () => {
     count++;
     displayCount()
+    checkCountValue();
+}
+
+const resetCount = () => {
+    if (count > 0) {
+        document.getElementById('countDisplay').innerHTML = 0;
+        alert('The Followers count has been reset')
+    }
 }
 
 const displayCount = () => {
     //  Display count in the HTML
     document.getElementById('countDisplay').innerHTML = count;
-    checkCountValue();
 
 }
 
@@ -17,5 +24,5 @@ const checkCountValue = () => {
         alert('Your Instagram post gained 10 followers!')
     } else if (count === 20) {
         alert('Your Instagram post gained 20 followers! Keep it up!!!')
-    }
+    } 
 }
