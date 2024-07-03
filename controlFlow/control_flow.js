b79a19fa83b3e560badfb71ff34637fa27bf0ede
KwabSA <kwabsa.dev@gmail.com>
2024-07-03 07:27:55 -0400

    Implemented control flow and conditional statements

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,54 @@
+let userRole = 'admin';
+let accessLevel;
+
+if (userRole === 'admin') {
+    accessLevel = 'Full access granted';
+} else if (userRole === 'manager') {
+    accessLevel = 'Limited access granted';
+} else {
+    accessLevel = 'No access granted';
+}
+
+console.log('Access Level: ', accessLevel)
+
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === 'admin') {
+        userMessage = 'Welcome Admin!';
+    } else {
+        userMessage = 'Welcome User!';
+    }
+} else {
+    userMessage = "Please login to access the system";
+}
+
+console.log('User Message: ', userMessage);
+
+// Defining variables and switch statement for User Type and User Category
+let userType = 'subscriber';
+let userCategory;
+
+switch (userType) {
+    case 'admin':
+        userCategory = 'Administrator';
+        break;
+    case 'manager':
+        userCategory = 'Manager';
+        break;
+    case 'subscriber':
+        userCategory = 'Subscriber';
+        break;
+    default:
+        userCategory = 'Unknown';
+}
+
+console.log('User Category: ', userCategory);
+
+// Using Ternary Operator for isAuthenticated and authenticationStatus
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? 'Authenticated' : 'Not Authenticated';
+
+console.log('Authentication Status: ', authenticationStatus);
