3ec8b52fed01830e3d2fca619060749545cd37f4
KwabSA <kwabsa.dev@gmail.com>
2024-07-06 16:35:04 -0400

    Added book details

---
+++ bookSystem/book_system.js
@@ -0,0 +1,48 @@
+let books = [];
+
+// Add book details
+const addBook = () => {
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDescription').value;
+    const pageNumbers = parseInt(document.getElementById('pageNumbers').value);
+
+    // Validate data entries and add to books array
+    if (bookName && authorName && bookDescription && !isNaN(pageNumbers)) {
+        
+        // Create book object
+        const book = {
+            name: bookName,
+            author: authorName,
+            description: bookDescription,
+            pages: pageNumbers
+        };
+
+        // Add book object to books
+        books.push(book);
+        showBooks();
+        clearInputs();
+    } else {
+        alert('Please fill all fields correctly.');
+    }
+}
+
+const showBooks = () => {
+    const booksDiv = books.map((book, index) => 
+    `<h1> Book ${index + 1}: </h1>
+    <p><strong> Book Name: </strong> ${book.name}</p>
+    <p><strong> Author Name: </strong> ${book.author}</p>
+    <p><strong> Book Description: </strong> ${book.description}</p>
+    <p><strong> No. of Pages: </strong> ${book.pages}</p>` 
+    );
+
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+// Clear the book details
+const clearInputs = () => {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pageNumbers').value = '';
+}
